75c04fa85850f07334823e9e4c73c6f1e25706c9
Tijo <hamsen3880@outlook.com>
2025-03-03 17:25:56 +0530

    navbar changes mobile and desktop

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Menu, School } from 'lucide-react'
+import { Menu, School, University, UniversityIcon, Webhook } from 'lucide-react'
 import React, { useEffect } from 'react'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
 import { Sheet, SheetClose, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
@@ -30,10 +30,10 @@ const Navbar = () => {
   return (
     <div className='h-16 border-b bg-background dark:border-b-secondary border-b-secondary fixed top-0 left-0 right-0 duration-300 z-50'>
 			<div className='max-w-7xl mx-auto hidden md:flex justify-between items-center gap-10 h-full'>
-        <div className='flex items-center gap-2'>
-          <School size={'30'} />
+        <div className='flex text-primary items-center gap-2'>
+          <Webhook size={'30'} />
           <Link to='/'>
-            <h1 className='hidden md:block font-extrabold text-2xl'>techschool.com</h1>
+            <h1 className='text-black dark:text-white hidden md:block font-extrabold text-2xl'>Techno Hooks</h1>
           </Link>     
         </div>
         <div className='flex items-center gap-4'>
@@ -83,7 +83,12 @@ const Navbar = () => {
         </div>
       </div>
       <div className='flex md:hidden items-center justify-between px-4 h-full'>
-        <h1 className='font-extrabold text-2xl'>LMS</h1>
+        <div className='flex text-primary items-center gap-2'>
+          <Webhook size={30} />
+          <Link to='/'>
+            <h1 className='text-black dark:text-white font-extrabold text-2xl'>Techno Hooks</h1>
+          </Link>     
+        </div>
         <MobileNavbar />
       </div>
     </div>
@@ -99,7 +104,7 @@ const MobileNavbar = () => {
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button size='icon' className='rounded-full bg-gray-100 hover:bg-gray-200' variant="outline">
+        <Button size='icon' className='rounded-full ' variant="outline">
           <Menu />
         </Button>
       </SheetTrigger>
