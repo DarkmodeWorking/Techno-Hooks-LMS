034ceeec0f6f9a97ac14c369982d67ef10153983
Tijo <hamsen3880@outlook.com>
2025-03-15 01:33:25 +0530

    progress redesign

--- Client/src/pages/client/CourseProgress.jsx
+++ Client/src/pages/client/CourseProgress.jsx
@@ -74,70 +74,78 @@ const CourseProgress = () => {
     <div className='max-w-7xl mx-auto p-4 mt-20'>
       <div className='flex justify-between mb-4'>
         <h1 className='text-2xl font-bold'>{courseTitle}</h1>
-        <Button onClick={completed ? handleIncompleteCourse : handleCompleteCourse} variant={completed ? 'outline' : 'default'}>
-          {
-            completed ? (
-              <div className='flex items-center'> 
-                <CheckCircle className='h-4 w-4 mr-2' /> <span>Completed</span>
-              </div>
-            ) : (
-              'Mark as completed'
-            ) 
-          }
+        <Button
+          onClick={completed ? handleIncompleteCourse : handleCompleteCourse}
+          variant={completed ? 'outline' : 'default'}
+        >
+          {completed ? (
+            <div className='flex items-center'>
+              <CheckCircle className='h-4 w-4 mr-2' /> <span>Completed</span>
+            </div>
+          ) : (
+            'Mark as completed'
+          )}
         </Button>
-      </div>  
+      </div>
       <div className='flex flex-col md:flex-row gap-6'>
         <div className='flex-1 md:w-3/5 h-fit rounded-lg shadow-lg p-4'>
           <div>
-            <video 
-              src={currentLecture?.videoUrl || initialLecture.videoUrl} 
+            <video
+              src={currentLecture?.videoUrl || initialLecture.videoUrl}
               controls
               className='w-full h-auto md:rounded-lg'
-              onPlay={() => handleLectureProgress(currentLecture?._id || initialLecture._id)}
-              
+              onPlay={() =>
+                handleLectureProgress(currentLecture?._id || initialLecture._id)
+              }
             />
           </div>
           <div className='mt-2'>
             <h3 className='font-medium text-lg'>
-              {
-                `Lecture ${courseDetails.lectures.findIndex((lec) => lec._id === (currentLecture?._id || initialLecture._id)) + 1} - ${currentLecture?.lectureTitle || initialLecture.lectureTitle}`
-              }
+              {`Lecture ${
+                courseDetails.lectures.findIndex(
+                  (lec) => lec._id === (currentLecture?._id || initialLecture._id)
+                ) + 1
+              } - ${currentLecture?.lectureTitle || initialLecture.lectureTitle}`}
             </h3>
           </div>
         </div>
-        <div className='flex flex-col w-full md:w-2/5 border-t md:border-t-0 md:border-l border-secondary md:pl-4 pt-4 md:pt-0'>
-          <h2 className='font-semibold text-xl mb-4'>Course Lecture</h2>
-          <div className='flex-1 overflow-y-auto'>
-            {
-              courseDetails?.lectures.map((lecture) => (
-                <Card key={lecture._id} className={`mb-3 hover:cursor-pointer transition transform ${lecture._id === currentLecture?._id ? 'bg-border' : 'bg-card'}`} onClick={() => handleSelectLecture(lecture)}>
-                  <CardContent className='flex items-center justify-between p-4'>
-                    <div className='flex items-center'>
-                      {
-                        isCompleted(lecture._id) ? (
-                          <CheckCircle2 size={24} className='text-primary mr-2' />
-                        ) : (
-                          <CirclePlay size={24} className='mr-2' />
-                        )
-                      }
-                      <div>
-                        <CardTitle className='text-lg font-medium'>{lecture.lectureTitle}</CardTitle>
-                      </div>
+        <div className='w-full md:w-2/5 flex-none h-[500px] border-t md:border-t-0 md:border-l border-secondary md:pl-4 pt-4 md:pt-0'>
+          <h2 className='font-semibold text-xl mb-4'>Course Lectures</h2>
+          <div className='h-[500px] overflow-y-auto rounded-lg border p-5 shadow-lg scrollbar'>
+            {courseDetails?.lectures.map((lecture) => (
+              <Card
+                key={lecture._id}
+                className={`mb-3 hover:cursor-pointer transition transform ${
+                  lecture._id === currentLecture?._id ? 'bg-border' : 'bg-card'
+                }`}
+                onClick={() => handleSelectLecture(lecture)}
+              >
+                <CardContent className='flex items-center justify-between p-4'>
+                  <div className='flex items-center'>
+                    {isCompleted(lecture._id) ? (
+                      <CheckCircle2 size={24} className='text-primary mr-2' />
+                    ) : (
+                      <CirclePlay size={24} className='mr-2' />
+                    )}
+                    <div>
+                      <CardTitle className='text-lg font-medium'>
+                        {lecture.lectureTitle}
+                      </CardTitle>
                     </div>
-                    {
-                      isCompleted(lecture._id) && (
-                        <Badge variant={'outline'}>Completed</Badge>
-                      )
-                    }                   
-                  </CardContent>
-                </Card>
-              ))
-            }
+                  </div>
+                  {isCompleted(lecture._id) && (
+                    <Badge variant={'outline'}>Completed</Badge>
+                  )}
+                </CardContent>
+              </Card>
+            ))}
           </div>
         </div>
       </div>
     </div>
   )
+  
+  
 }
 
 export default CourseProgress
